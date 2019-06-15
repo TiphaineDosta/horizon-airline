b5dc241d3e753e37c009532845c086c2538a3b16
Mendy Dihars <mendy.dihars@laposte.net>
2019-06-15 20:47:08 +0200

    prepare for tab changing text

--- src/javascript/tabs.js
+++ src/javascript/tabs.js
@@ -3,5 +3,12 @@ tabs.forEach(tab => {
     tab.addEventListener('click', e => {
         document.querySelectorAll('.active')[0].classList.remove('active');
         tab.classList.add('active');
+        document.querySelectorAll('.item-active').forEach(el => {
+            el.classList.add('item');
+            el.classList.remove('item-active');
+        });
+        document.querySelectorAll('.' + e.target.dataset.tab).forEach(el => {
+            el.classList.add("item-active");
+        })
     })
 })
