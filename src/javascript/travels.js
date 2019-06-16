aee1a4730b9d1280292564d4f9c14d242bcbd430
Mendy Dihars <mendy.dihars@laposte.net>
2019-06-16 19:56:42 +0200

    add effects on buttons and radio buttons

--- src/javascript/travels.js
+++ src/javascript/travels.js
@@ -4,9 +4,21 @@ document.querySelectorAll('.travel').forEach(travel => {
             let target = e.path.find(el => el.className.includes("travel"))
             if (el.dataset.travel == target.dataset.travel) {
                 el.classList.remove('travel-active');
+                el.childNodes[3].childNodes[7].innerHTML = '<div class=\'btn-orange-alter\'>Book</div>'
             };
         })
         travel.classList.add('travel-active');
-        document.getElementById('payment').style= "display: block;"
+        travel.childNodes[3].childNodes[7].innerHTML = '<div class=\'btn-orange-alter\'>Booked</div>'
+        document.getElementById('payment').removeAttribute('hidden')
+    })
+})
+
+document.querySelectorAll('.radio').forEach(el => {
+    el.addEventListener('change', e => {
+        if (e.target.id == "fid-no" && e.target.checked) {
+            document.getElementById('merch-fid').style = "display: block;"
+        } else {
+            document.getElementById('merch-fid').style = "display: none;"
+        } 
     })
 })
